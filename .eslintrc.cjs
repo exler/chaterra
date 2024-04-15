1db5e602e52f278a749b986624b07c357e085bda
Kamil Marut <kamil@kamilmarut.com>
2024-04-15 22:24:06 +0100

    build: Extend ESLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,21 @@ module.exports = {
     env: { browser: true, es2020: true },
     extends: [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-type-checked",
+        "plugin:@typescript-eslint/stylistic-type-checked",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
         "plugin:react-hooks/recommended",
         "prettier"
     ],
     ignorePatterns: ["dist", ".eslintrc.cjs"],
     parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: ["./tsconfig.json", "./tsconfig.node.json"],
+        tsconfigRootDir: __dirname
+    },
     plugins: ["react-refresh", "simple-import-sort", "import"],
     rules: {
         "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
@@ -17,5 +26,8 @@ module.exports = {
         "import/first": "error",
         "import/newline-after-import": "error",
         "import/no-duplicates": "error"
+    },
+    settings: {
+        react: { version: "detect" }
     }
 };
