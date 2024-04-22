e49cb826f564437be7e14270403bf1fa69c7bcde
Kamil Marut <kamil@kamilmarut.com>
2024-04-22 23:26:42 +0100

    feat(app): Add various info about app

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -10,8 +10,9 @@ interface SideMenuProps {
 export default function SideMenu({ className, children }: SideMenuProps) {
     return (
         <div className={twMerge("flex flex-col", className)}>
-            <div className="mx-auto">
+            <div className="mx-auto flex flex-row items-center gap-4">
                 <img src={SiteLogo} alt="Chaterra" height={50} width={160} />
+                <div className="badge badge-warning font-bold text-xs cursor-default">PRE-ALPHA</div>
             </div>
             {children}
         </div>
--- src/routes/Settings.tsx
+++ src/routes/Settings.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { FaKey } from "react-icons/fa6";
+import { IoCheckmarkCircle } from "react-icons/io5";
 
 import { useUserSettingsStore } from "@/stores/userSettings";
 
@@ -9,39 +10,60 @@ interface FormData {
 }
 
 export default function Settings() {
-    const { openAIApiKey, setOpenAIApiKey } = useUserSettingsStore();
-
     const { register, handleSubmit, setValue } = useForm<FormData>();
+    const [showUpdated, setShowUpdated] = useState(false);
+
+    const { openAIApiKey, setOpenAIApiKey } = useUserSettingsStore();
 
     useEffect(() => {
         setValue("openAIApiKey", openAIApiKey);
     }, [setValue, openAIApiKey]);
 
+    const handleShowUpdated = () => {
+        setShowUpdated(true);
+        setTimeout(() => {
+            setShowUpdated(false);
+        }, 3000);
+    };
+
     const onSubmit: SubmitHandler<FormData> = (data) => {
         setOpenAIApiKey(data.openAIApiKey);
+        handleShowUpdated();
     };
 
     return (
-        <div className="flex flex-col items-center justify-center gap-8">
-            <h1 className="font-bold text-2xl">Settings</h1>
-            <section className="w-1/3">
-                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
-                    <label className="form-control">
-                        <div className="label">
-                            <span className="label-text">OpenAI API Key</span>
-                        </div>
+        <div className="flex flex-col items-center h-full justify-between">
+            <div className="flex flex-col items-center gap-8 w-full">
+                <h1 className="font-bold text-2xl">Settings</h1>
+                <section className="w-1/3">
+                    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+                        {showUpdated && (
+                            <div role="alert" className="alert alert-success">
+                                <IoCheckmarkCircle size="1.5rem" />
+                                <span>Your settings have been updated!</span>
+                            </div>
+                        )}
 
-                        <div className="input input-bordered input-sm flex items-center gap-2">
-                            <FaKey size="1rem" />
-                            <input type="text" className="grow" {...register("openAIApiKey")} />
-                        </div>
-                    </label>
+                        <label className="form-control">
+                            <div className="label">
+                                <span className="label-text">OpenAI API Key</span>
+                            </div>
 
-                    <button type="submit" className="btn btn-sm btn-primary">
-                        Update
-                    </button>
-                </form>
-            </section>
+                            <div className="input input-bordered input-sm flex items-center gap-2">
+                                <FaKey size="1rem" />
+                                <input type="text" className="grow" {...register("openAIApiKey")} />
+                            </div>
+                        </label>
+
+                        <button type="submit" className="btn btn-sm btn-primary">
+                            Update
+                        </button>
+                    </form>
+                </section>
+            </div>
+            <p className="opacity-80">
+                Data is stored locally in your browser. Keys are only used to communicate with OpenAI.
+            </p>
         </div>
     );
 }
